3a27530b5e5787305569fff750bc9f9908b8e123
shamalmagdum13 <shamalmagdum1311@gmail.com>
2024-08-01 00:48:42 +0530

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
--- src/components/TollList.js
+++ src/components/TollList.js
@@ -1,4 +1,4 @@
-import {React} from 'react';
+import React from 'react';
 import './VehicleList.css';
 import './TollList.css';
 
